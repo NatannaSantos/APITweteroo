4ae251913df24885446e403cfd7572cc026a8e02
Natanna <natannacortez@gmail.com>
2023-01-21 17:11:56 -0300

    init project

---
+++ src/main/java/com/tweteroo/api/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.tweteroo.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sign-up")
+public class UserController {
+
+    public String createUser(){
+       
+        
+    }
+}
